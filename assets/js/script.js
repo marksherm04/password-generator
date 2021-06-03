529995257854e606a2ab7396691a46ab9611d083
Mark S <marksherm04@gmail.com>
2021-06-03 17:46:33 -0400

    added arrays for letters, numbers and special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,18 @@ var startPassword = function(){
 
 startPassword();
 
+// Arrays
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "#", "$", "%", "&", "*", "+", "@", "^", "~"]
+upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
 var passwordLength = window.prompt("Input password length between 8-128"); 
 
 var minLength = passwordLength;
 var maxLength = passwordLength;
+
 
 // accept only number input from numbers between 8-128
 if (minLength < 8) {
@@ -21,7 +29,6 @@ if (minLength < 8) {
 };
 
 var characterTypes = window.prompt("Choose your charater types");
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
