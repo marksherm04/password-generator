c3101c9e5e3bcd7ae3bd0c79aeb6854b6fda9f6d
Mark S <marksherm04@gmail.com>
2021-06-10 13:08:32 -0400

    added comments to different selections explaining how code works, ready to sumbit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // length of password for first if statement
 var randomPassword;
-// selection by user to see which selections are made for arrays & other var used below
+// var selections holds empty array since multiple arrays being input down below in selections
 var selections = [];
 var numbers;
 var characters;
@@ -31,13 +31,11 @@ function generatePassword() {
     return;
     // TO DO - I WANT THIS TO LOOP IF NO SELECTION IS MADE !!!! //
   }
-  // if password < 8 or password is > 128 then alert returns they must select betweeen 8-128 characters
+  // if password < 8 or password is > 128 then alert returns they must select betweeen 8-128 characters and returned to character input
   else if (randomPassword < 8 || randomPassword > 128) {
     randomPassword = parseInt(alert("Error!   Password length must be between 8-128 characters!"));
     writePassword();
     return;
-    // TO DO - I WANT THIS TO LOOP IF NO SELECTION IS MADE !!!! //
-
     // otherwise the confirm prompts will start and cycle through all 4 options
   } else {
     var numbers = confirm("Do you want to use numbers?")
@@ -46,37 +44,41 @@ function generatePassword() {
     var lowerCase = confirm("Do you want lower case letters?");
   };
 
+  // if statements of refactored code for user selection.  First if statement gives all selections via OR operator
   if (numbers || characters || upperCase || lowerCase) {
-
+  // if numbers are selected, it will concatenate with the other selections made by user
     if (numbers) {
       selections = selections.concat(numericCharacters);
     }
-
+    // if characters are selected, it will concatenate with the other selections made by user
     if (characters) {
       selections = selections.concat(specialCharacters);
     }
-
+    // if upperCase letters are selected, it will concatenate with the other selections made by user
     if (upperCase) {
       selections = selections.concat(upperCaseLetters);
     }
-
+    // if lowerCase letters are selected, it will concatenate with the other selections made by user
     if (lowerCase) {
       selections = selections.concat(lowerCaseLetters);
     }
   }
+  // if no selections are made the alert has the user select one choice, and returns to original character amount input
   else {
-    alert("Please select one of the choices!");
+    alert("Please select at least one of the choices!");
     writePassword();
     return;
   }
 
-  // // for loop to get password created
+  // for loop to get password created, var mixCharaters listed as empty array to hold random array characters selected
+  // the .push method adds elements to the end of an array and returns the new array length which is Math.random slections
   var mixCharacters = [];
   var i;
   for (i = 0; i < randomPassword; i++) {
     mixCharacters.push(selections[Math.floor(Math.random() * selections.length)]);
   }
-  var password = mixCharacters.join('');
+  // used .join ("") instead of string array to remove commas between random password characters, then returned password
+  var password = mixCharacters.join("");
   return (password);
 };
 
